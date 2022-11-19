adb966c5fb1981591586291c76a0861a292f28a1
ubbl-cmd <aleksej.sapojnikov@gmail.com>
2022-11-19 13:15:10 +0300

    lb4

---
+++ build/CMakeFiles/oop.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/egunuraka/Desktop/leti/oop/oop/src/Controller.cpp" "CMakeFiles/oop.dir/src/Controller.cpp.o" "gcc" "CMakeFiles/oop.dir/src/Controller.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/Game.cpp" "CMakeFiles/oop.dir/src/Game.cpp.o" "gcc" "CMakeFiles/oop.dir/src/Game.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Entities/Player/Player.cpp" "CMakeFiles/oop.dir/src/GameLogic/Entities/Player/Player.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Entities/Player/Player.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Events/AbstractMapEvent.cpp" "CMakeFiles/oop.dir/src/GameLogic/Events/AbstractMapEvent.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Events/AbstractMapEvent.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Events/AbstractPlayerEvent.cpp" "CMakeFiles/oop.dir/src/GameLogic/Events/AbstractPlayerEvent.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Events/AbstractPlayerEvent.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Events/AllKeysEvent.cpp" "CMakeFiles/oop.dir/src/GameLogic/Events/AllKeysEvent.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Events/AllKeysEvent.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Events/ExitEvent.cpp" "CMakeFiles/oop.dir/src/GameLogic/Events/ExitEvent.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Events/ExitEvent.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Events/KeyEvent.cpp" "CMakeFiles/oop.dir/src/GameLogic/Events/KeyEvent.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Events/KeyEvent.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Events/PassMapEvent.cpp" "CMakeFiles/oop.dir/src/GameLogic/Events/PassMapEvent.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Events/PassMapEvent.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Events/TrapEvent.cpp" "CMakeFiles/oop.dir/src/GameLogic/Events/TrapEvent.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Events/TrapEvent.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Field/Cell.cpp" "CMakeFiles/oop.dir/src/GameLogic/Field/Cell.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Field/Cell.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Field/Field.cpp" "CMakeFiles/oop.dir/src/GameLogic/Field/Field.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Field/Field.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/GameLogic/Field/Move.cpp" "CMakeFiles/oop.dir/src/GameLogic/Field/Move.cpp.o" "gcc" "CMakeFiles/oop.dir/src/GameLogic/Field/Move.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/IO/CommandReader.cpp" "CMakeFiles/oop.dir/src/IO/CommandReader.cpp.o" "gcc" "CMakeFiles/oop.dir/src/IO/CommandReader.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/IO/Config/ConfigReader.cpp" "CMakeFiles/oop.dir/src/IO/Config/ConfigReader.cpp.o" "gcc" "CMakeFiles/oop.dir/src/IO/Config/ConfigReader.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/IO/Config/FileConfigReader.cpp" "CMakeFiles/oop.dir/src/IO/Config/FileConfigReader.cpp.o" "gcc" "CMakeFiles/oop.dir/src/IO/Config/FileConfigReader.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/IO/FieldWriter.cpp" "CMakeFiles/oop.dir/src/IO/FieldWriter.cpp.o" "gcc" "CMakeFiles/oop.dir/src/IO/FieldWriter.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/IO/Loggers/AbstractLogger.cpp" "CMakeFiles/oop.dir/src/IO/Loggers/AbstractLogger.cpp.o" "gcc" "CMakeFiles/oop.dir/src/IO/Loggers/AbstractLogger.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/IO/Loggers/ConsoleLogger.cpp" "CMakeFiles/oop.dir/src/IO/Loggers/ConsoleLogger.cpp.o" "gcc" "CMakeFiles/oop.dir/src/IO/Loggers/ConsoleLogger.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/IO/Loggers/FileLogger.cpp" "CMakeFiles/oop.dir/src/IO/Loggers/FileLogger.cpp.o" "gcc" "CMakeFiles/oop.dir/src/IO/Loggers/FileLogger.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/IO/Views/CellView.cpp" "CMakeFiles/oop.dir/src/IO/Views/CellView.cpp.o" "gcc" "CMakeFiles/oop.dir/src/IO/Views/CellView.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/IO/Views/FieldView.cpp" "CMakeFiles/oop.dir/src/IO/Views/FieldView.cpp.o" "gcc" "CMakeFiles/oop.dir/src/IO/Views/FieldView.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/Obsersers/AbstractObservable.cpp" "CMakeFiles/oop.dir/src/Obsersers/AbstractObservable.cpp.o" "gcc" "CMakeFiles/oop.dir/src/Obsersers/AbstractObservable.cpp.o.d"
+  "/home/egunuraka/Desktop/leti/oop/oop/src/Obsersers/AbstractObserver.cpp" "CMakeFiles/oop.dir/src/Obsersers/AbstractObserver.cpp.o" "gcc" "CMakeFiles/oop.dir/src/Obsersers/AbstractObserver.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
